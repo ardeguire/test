d0ccbdd4fff3b66472c8c4db5bb831a55581a29f
Adam De Guire <adam@ardeguire.com>
2020-09-21 19:21:50 +0000

    make some temporary car fixtures

---
+++ app/src/DataFixtures/CarFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Make;
+use App\Entity\Model;
+
+class MakeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $make = new Make();
+        $make->setName("Ford");
+
+        $model = new Model();
+        $model->setName("Focus");
+        $model->setMake($make);
+
+        //$make->hasModel($model)
+
+        $manager->persist($make);
+        $manager->persist($model);
+
+        $manager->flush();
+    }
+}
