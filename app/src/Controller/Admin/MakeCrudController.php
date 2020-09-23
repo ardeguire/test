1d5acc2af623c66d0f2cf76787f14de53f1cc205
Adam De Guire <adam@ardeguire.com>
2020-09-23 19:56:03 +0000

    enable detail view for each entity

--- app/src/Controller/Admin/MakeCrudController.php
+++ app/src/Controller/Admin/MakeCrudController.php
@@ -6,6 +6,9 @@ use App\Entity\Make;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class MakeCrudController extends AbstractCrudController
 {
@@ -23,4 +26,8 @@ class MakeCrudController extends AbstractCrudController
         ];
     }
     
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
 }
--- app/src/Controller/Admin/ModelCrudController.php
+++ app/src/Controller/Admin/ModelCrudController.php
@@ -6,6 +6,9 @@ use App\Entity\Model;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 
 class ModelCrudController extends AbstractCrudController
@@ -22,6 +25,11 @@ class ModelCrudController extends AbstractCrudController
             TextField::new('name'),
             AssociationField::new('make'),
         ];
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
     
 }
